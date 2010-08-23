0c372b93289cdc03ea9b40ebdad8046db1c34606
Artie Copeland <artie@yestech.org>
2010-08-22 21:49:53 -0700

    added license, description

--- cassandra-gen-tokens.py
+++ cassandra-gen-tokens.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+#
+# A simple script to generate a set of tokens that can be used in a apache cassandra cluster.
+#
+# The scripts asks for a cluster size then prints that many keys
+#
+# Based on benjamin black cassandra summit talk (http://www.slideshare.net/benjaminblack/cassandra-summit-2010-operations-troubleshooting-intro).
+# Author: Artie Copeland <artie@yestech.org>
+# Distributed under the GNU General Public License, version 3.0.
+#
+
 nodes = int(raw_input("How many nodes in the cluster -> "))
 limit = nodes
 for n in range(limit):
