c57b091db6d6c8bbe5c69772912bf1a63ad31426
Artie Copeland <artie@yestech.org>
2010-08-24 17:00:43 -0700

    added dependency to comment

--- cassandra-gen-tokens.py
+++ cassandra-gen-tokens.py
@@ -2,6 +2,7 @@
 
 #
 # A simple script to generate a set of tokens that can be used in a apache cassandra cluster.
+# The script relys on the module "cassandrautil" to be available.
 #
 # The scripts asks for a cluster size then prints that many keys
 # 
@@ -9,6 +10,8 @@
 # Author: Artie Copeland <artie@yestech.org>
 # Distributed under the GNU General Public License, version 3.0.
 # Latest version: http://github.com/yestech/yessandbox/blob/master/cassandra-gen-tokens.py
+#
+# 
 #
 
 from cassandrautil import CassandraTokens
